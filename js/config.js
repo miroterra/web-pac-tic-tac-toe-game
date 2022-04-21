96502a455cd5c7dace6ca8d1417616312618918a
Takphantom <miroterra6@gmail.com>
2022-04-22 00:13:55 +0900

    이동

--- js/config.js
+++ js/config.js
@@ -1,4 +1,5 @@
-function openPlayerConfig() {
+function openPlayerConfig(event) {
+  const clickedButton = event.target.dataset.playerid;
   playerConfigOverlayElement.style.display = 'block';
   backdropElement.style.display = 'block';
 }
@@ -6,10 +7,18 @@ function openPlayerConfig() {
 function closePlayerConfig() {
   playerConfigOverlayElement.style.display = 'none';
   backdropElement.style.display = 'none';
+  formElement.firstElementChild.classList.remove('error');
+  errorsOutputElement.textContent = '';
 }
 
 function savePlayerConfig(event) {
   event.preventDefault();
   const formData = new FormData(event.target);
-  const enteredPlayername = formData.get('playername');
+  const enteredPlayername = formData.get('playername').trim();
+
+  if (!enteredPlayername) {
+    event.target.firstElementChild.classList.add('error');
+    errorsOutputElement.textContent = 'Please enter a valid name';
+    return;
+  }
 }
