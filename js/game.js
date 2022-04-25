691043315664594d51158302bb738873a3c97fdf
Takphantom <miroterra@naver.com>
2022-04-25 22:42:55 +0900

    이동

---
+++ js/game.js
@@ -0,0 +1,34 @@
+function startNewGame() {
+  if (players[0].name === '' || players[1].name === '') {
+    alert('Please set custom player names for both player');
+    return;
+  }
+  activePlayerNameElement.textContent = players[activePlayer].name;
+  gameAreaElement.style.display = 'block';
+}
+
+function switchPlayer() {
+  if (activePlayer === 0) {
+    activePlayer = 1;
+  } else {
+    activePlayer = 0;
+  }
+  activePlayerNameElement.textContent = players[activePlayer].name;
+}
+
+function selectGameField(event) {
+  const selectedField = event.target;
+  const selectedColumn = selectedField.dataset.col - 1;
+  const selectedRow = selectedField.dataset.row - 1;
+
+  if (gameData[selectedRow][selectedColumn] > 0) {
+    alert('Please select an empty field');
+  }
+
+  selectedField.textContent = players[activePlayer].symbol;
+  selectedField.classList.add('disabled');
+
+  gameData[selectedRow][selectedColumn] = activePlayer + 1;
+
+  switchPlayer();
+}
